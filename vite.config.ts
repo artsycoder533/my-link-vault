ca55e6d8b37d47a08ab480c81af7327e2ed4b7f8
Natasha Johnson <njohnson533@gmail.com>
2024-03-28 14:24:16 -0400

    chore: update config files

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // test: {
+  //   // some paths to the files that are test files
+  //   include: ["./**/*.test.ts", "./**/*.test.tsx"],
+  //   globals: true,
+  //   environment: "jsdom",
+  //   // setupFiles: "./__tests__/setup"
+  // },
 })
